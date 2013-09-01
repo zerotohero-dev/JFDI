a1a9cdf9e4c4d6f23669387b9180a81dfff1a66a
Volkan Özçelik <volkan@o2js.com>
2013-09-01 00:01:33 +0000

    organized the code into modules. updated jshintrc

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 #! /usr/bin/env node
 
+'use strict';
+
 /*
  *           ___         ___           ___
  *          /\  \       /\  \         /\  \          ___
@@ -13,440 +15,97 @@
  *                                   \::/__/      \/__/
  */
 
-// TODO: move these to jshint.rc
-/*global process, console, __dirname*/
-
-'use strict';
-
 var program = require('commander');
-var prompt = require('prompt');
-var path = require('path');
-var fs = require('fs');
 
 var JFDI = require('./lib/JFDI');
 
-var println = console.log;
 var info = function() {};
+var command = require('./lib/command');
+var parse = require('./lib/parsing').parseArguments;
 
-
-var rootPath = JFDI.getDataRoot();
-
-function handlePromptFailure() {
-    println('');
-    println('### Bummer! JFDI Path Setup Failed! ###');
-    println('');
-    println('An error occured. Retrying with root privileges may help.');
-    println('');
-}
-
-function checkPath() {
-    prompt.get(['path'], function (err, result) {
-        if (err) {
-            return handlePromptFailure(err);
-        }
-
-        var dir = result.path;
-
-        fs.stat(dir, function(err) {
-            if (err) {
-                if (err.code === 'ENOENT') {
-                    println('');
-                    println('No such directory exists.');
-                    println('Please create the directory first.');
-                    println('Then re-enter here.');
-                    println('');
-
-                    checkPath();
-
-                    return;
-                }
-
-                handlePromptFailure();
-            }
-
-            try {
-                fs.writeFileSync(path.join(__dirname, 'data/.root'), dir);
-
-                fs.writeFileSync(path.join(dir, '.today.timestamp'));
-                fs.writeFileSync(path.join(dir, 'done.txt'));
-                fs.writeFileSync(path.join(dir, 'today.txt'));
-                fs.writeFileSync(path.join(dir, 'tomorrow.txt'));
-            } catch(err2) {
-                handlePromptFailure();
-
-                return;
-            }
-
-            println('');
-            println('### Yay! ####');
-            println('');
-            println('    Ready to go! You can use JFDI now.');
-            println('');
-            println('    Visit');
-            println('');
-            println('       https://github.com/v0lkan/JFDI/blob/master/README.md');
-            println('');
-            println('    for usage examples.');
-            println('');
-        });
-    });
-}
-
-if (!rootPath) {
-    println('');
-    println('### Set Your JFDI for the First Time ###');
-    println('');
-    println('    It looks like this is the first time you are using JFDI.');
-    println('    Dont\'t worry, it\'s easy.');
-    println('');
-    println('    The only thing you need to configure is a folder to store');
-    println('    your JFDI data.');
-    println('');
-    println('    Where do you want to store your JFDI data?');
-    println('    Enter the full path ( like: /home/kenobi/Dropbox/JFDI/ ).');
-    println('');
-
-    prompt.start();
-
-    checkPath();
-
-    return;
-}
-
-function pad(index) {
-    var str = '' + index;
-
-    switch (str.length) {
-        case 1:
-            return '   ' + str;
-        case 2:
-            return '  ' + str;
-        case 3:
-            return ' ' + str;
-        case 4:
-            return str;
-    }
-}
-
-function printGoals(goals) {
-    goals.forEach(function(goal, index) {
-        println(pad(index) + ' ' + goal);
-    });
-}
-
-function handleCallbackResult(error, goals) {
-    println('');
-
-    if (error) {
-        println('JFDI failed to add the goal :(');
-
-        return;
-    }
-
-    var realm = goals.realm;
-    var query = goals.query;
-
-    switch (realm) {
-        case 'today':
-            if (query) {
-                println('   ### "' + query + '" for Today ###');
-
-                break;
-            }
-
-            println('   ### JFDI List For Today ###');
-
-            break;
-
-        case 'tomorrow':
-            if (query) {
-                println('   ### "' + query + '" for Today ###');
-
-                break;
-            }
-
-            println('   ### Upcoming JFDI Stuff ###');
-
-            break;
-
-        default:
-            println('Unknown realm "' + realm + '". exiting.');
-
-            return;
-    }
-
-    goals = goals.goals;
-
-    if (goals.length === 0) {
-        switch (realm) {
-            case 'today':
-                if (query) {
-                    println('');
-                    println('       There are no matching goals for your query "' + query + '" for today :(');
-                    println('');
-
-                    break;
-                }
-
-                println('');
-                println('       *Zero Inbox* for today! Hooray!');
-                println('');
-                println('       Sample Usage:');
-                println('           Add a Goal       : jfdi [-a] "Save the world; one goal at a time."');
-                println('           List Goals       : jfdi -l');
-                println('           List All Commands: jfdi -h');
-                println('');
-
-                break;
-            case 'tomorrow':
-                if (query) {
-                    println('');
-                    println('       There are no matching goals for your query "' + query + '" for the near future :(');
-                    println('');
-
-                    break;
-                }
-
-                println('');
-                println('       Your foreseeable future is pretty blank. Why not add some tasks?');
-                println('');
-                println('       Sample Usage:');
-                println('           Add a Goal       : jfdi -a "Save the world; one goal at a time."');
-                println('           List Goals       : jfdi -l');
-                println('           List All Commands: jfdi -h');
-                println('');
-
-                break;
-            default:
-                println('Unknown realm "' + realm + '". exiting.');
-
-                break;
-        }
-
-        return;
-    }
-
-    println('');
-    printGoals(goals);
-    println('');
-}
-
-// If you define a new command or option, make sure you update this, too.
-// If a command is NOT a special command, then it means that it will implicitly
-// called with add.
-// i.e.
-//
-//      jfdi "Save the cheerleader; save the world!"
-//
-// will be implicityl converted to
-//
-//      jfdi --add "Save the cheerleader; save the world!"
-//
-// If the text is in this list, then this implicit conversion will not be done.
-// So
-//
-//      jfdi today
-//
-// will be
-//
-//      jfdi --list today // note that we don't add `--add` now.
-var specialCommands = [
-    // TODO: be DRY, make them constants!
-    'today',
-    'tomorrow',
-    'all',
-    '-t', '--tomorrow',
-    '-h', '--help'
-];
-
-var knownCommands = [
-    // TODO: be DRY, make them constants!
-    'today',
-    'tomorrow',
-    'all',
-    '-t', '--tomorrow',
-    '-h', '--help',
-    '-V', '--version',
-    '-a', '--add',
-    '-f', '--find',
-    '-d', '--defer',
-    '-p', '--prioritize',
-    '-l', '--list',
-    '-t', '--tomorrow',
-    '-x', '--do'
-];
-
-function isSpecialCommand(initialCommand) {
-    return specialCommands.indexOf(initialCommand) > -1;
-}
-
-function isKnownCommand(command) {
-    return knownCommands.indexOf(command) > -1;
-}
+if (!JFDI.sanitize()) {return;}
 
 function init() {
-    var kNoArgs = 2;
-    var kSingleCommand = 3;
-    var kNotImplemented = 'This functionality has not been implemented yet.';
-    var args = process.argv;
-    var argLen = args.length;
+    info(process.argv);
 
-    var initialCommand = args[2];
+    parse(process.argv);
 
-    if (args.length === kSingleCommand && !isSpecialCommand(initialCommand)) {
-        args[3] = args[2];
-        args[2] = '--add';
-    } else if (argLen === kNoArgs) {
-        args.push('--list');
-        args.push('today');
-    } else if(args[2] === '-e' || args[2] === '--expedite') {
-        if(args[4] === undefined) {
-            args[4] = 'tomorrow';
-        }
-    } else if(args[2] === '-t' || args[2] === '--tomorrow') {
-        args[2] = '--list';
-        args[3] = 'tomorrow';
-
-        args.length = 4;
-    }
-
-    if(args.indexOf('today') === -1 && args.indexOf('tomorrow') === -1) {
-        args.push('today');
-    }
-
-    if(!isNaN(parseInt(args[2], 10))) {
-        args.splice(2, 0, '--do');
-    }
-
-    if (!isKnownCommand(args[2])) {
-        if (args.length > 4) {
-            args.splice(2, 0, '--add');
-            args[3] = args.splice(3, args.length - 4).join(' ');
-            args[4] = 'today';
-        }
-    } else if (args[2] === '-a' || args[2] === '--add') {
-        if (args.length > 4) {
-            args[3] = args.splice(3, args.length - 4).join(' ');
-            args[4] = 'today';
-        }
-    } else if (args[2] === '-f' || args[2] === '--find') {
-        if (args.length > 4) {
-            args[3] = args.splice(3, args.length - 4).join(' ');
-            args[4] = 'today';
-        }
-    }
-
-    println(process.argv);
+    info(process.argv);
 
     // TODO: auto replace it from package JSON
     program.version('0.0.1');
 
     program
-    .option('-a, --add <goal>', 'Add a goal to the top of the queue.');
+    .option(
+        '-a, --add <goal>',
+        'Add a goal to the top of the queue.'
+    );
 
     program
-    .option('-f, --find <keyword>', 'Search for goals.');
+    .option(
+        '-f, --find <keyword>',
+        'Search for goals.'
+    );
 
     program
-    .option('-d, --defer <id>', 'Move the goal with id <id> **from today\'s queue** to the top of tomorrow\'s queue.', parseInt);
+    .option(
+        '-d, --defer <id>',
+        'Move the goal with id <id> **from today\'s queue** ' +
+            'to the top of tomorrow\'s queue.',
+        parseInt
+    );
 
     program
-    .option('-e, --expedite <id>', 'Move the goal with id <id> **from tomorrow\'s queue** to the top of today\'s queue.', parseInt);
+    .option(
+        '-e, --expedite <id>',
+        'Move the goal with id <id> **from tomorrow\'s queue** ' +
+            'to the top of today\'s queue.',
+        parseInt
+    );
 
     program
-    .option('-p, --prioritize <id>', 'Move the goal with id <id> to the top of the queue.', parseInt);
+    .option(
+        '-p, --prioritize <id>',
+        'Move the goal with id <id> to the top of the queue.',
+        parseInt
+    );
 
     program
-    .option('-l, --list', 'List the goals in the queue.', parseInt);
+    .option(
+        '-l, --list',
+        'List the goals in the queue.',
+        parseInt
+    );
 
     program
-    .option('-t, --tomorrow', 'List tomorrow\'s goals.');
+    .option(
+        '-t, --tomorrow',
+        'List tomorrow\'s goals.'
+    );
 
     program
-    .option('-x, --do <id>', 'Completes the goal with the given id <id>.', parseInt);
+    .option(
+        '-x, --do <id>',
+        'Completes the goal with the given id <id>.',
+        parseInt
+    );
 
     program
     .command('today')
-    .description('If no option given, list the goals for today. Otherwise executes the option within the context of today.')
-    .action(function() {
-        var goal = program.add;
-        var query = program.find;
-        var later = program.defer;
-        var sooner = program.expedite;
-        var upper = program.prioritize;
-        var complete = program['do'];
-
-        if (goal) {
-            info('add');
-
-            JFDI.add(goal, 'today', handleCallbackResult);
-
-        } else if (query) {
-            info('find');
-
-            JFDI.find('today', query, handleCallbackResult);
-        } else if (later !== undefined) {
-            info('later');
-
-            JFDI.defer(later, handleCallbackResult);
-        } else if (sooner !== undefined) {
-            println('\n*** You cannot expedite a current goal. Did you mean to --prioritize it, instead?. Type `jfdi -h` for more info. ***\n');
-
-            JFDI.list('today', handleCallbackResult);
-        } else if (upper !== undefined) {
-            info('upper');
-
-            println(kNotImplemented + ' (prioritize)\n');
-
-            JFDI.list('today', handleCallbackResult);
-        } else if (complete !== undefined) {
-            info('complete');
-
-            JFDI.complete(complete, handleCallbackResult);
-        } else {
-            info('default');
-
-            JFDI.list('today', handleCallbackResult);
-        }
-    });
+    .description('If no option given, list the goals for today.' +
+        ' Otherwise executes the option within the context of today.')
+    .action(command.handleToday.bind(program));
 
     program
     .command('tomorrow')
-    .description('If no option given, list the goals for tomorrow. Otherwise executes the option within the context of tomorrow.')
-    .action(function() {
-        var goal = program.add;
-        var query = program.find;
-        var later = program.defer;
-        var sooner = program.expedite;
-        var upper = program.prioritize;
-        var complete = program['do'];
-
-        if (goal) {
-            // TODO: localization:
-            println('\n*** You cannot add a goal to tomorrow\'s queue. Add it to today\'s queue, then defer it. Type `jfdi -h` for more info. ***\n');
-        } else if (query) {
-
-            JFDI.find('tomorrow', query, handleCallbackResult);
-        } else if (later !== undefined) {
-            println('\n*** You cannot defer a future goal. Did you mean to --expedite it, instead? Type `jfdi -h` for more info. ***\n');
-        } else if (complete !== undefined) {
-            println('\n*** You cannot complete a future goal. Add it to today\'s queue, then defer it. Type `jfdi -h` for more info. ***\n');
-        } else if (sooner !== undefined) {
-            JFDI.expedite(sooner, handleCallbackResult);
-        } else if (upper !== undefined) {
-           println(kNotImplemented + ' (expedite/tw)\n');
-        } else {
-           JFDI.list('tomorrow', handleCallbackResult);
-        }
-    });
+    .description('If no option given, list the goals for tomorrow.' +
+        ' Otherwise executes the option within the context of tomorrow.')
+    .action(command.handleTomorrow.bind(program));
 
     program
     .command('all')
     .description('Lists today\'s and tomorrow\'s goals.')
-    .action(function() {
-        JFDI.list('today', handleCallbackResult);
-        JFDI.list('tomorrow', handleCallbackResult);
-    });
+    .action(command.handleAll.bind(program));
 
     program.parse(process.argv);
 }
